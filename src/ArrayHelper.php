7d76cdd00a2adf27f97700ea19e8b68239cc8334
aidan-casey <aidan@caseyhouse.net>
2024-10-06 20:02:49 +0000

    Merge pull request #396 from tempestphp/add-env-example

--- src/ArrayHelper.php
+++ src/ArrayHelper.php
@@ -4,196 +4,25 @@ declare(strict_types=1);
 
 namespace Tempest\Support;
 
-use ArrayAccess;
-use Closure;
-use Countable;
-use Generator;
-use Iterator;
-use Serializable;
-use function Tempest\map;
-
-/**
- * @template TValueType
- * @implements ArrayAccess<array-key, TValueType>
- */
-final class ArrayHelper implements Iterator, ArrayAccess, Serializable, Countable
+final readonly class ArrayHelper
 {
-    use IsIterable;
-
-    private array $array;
-
-    public function __construct(
-        mixed $input = [],
-    ) {
-        if (is_array($input)) {
-            $this->array = $input;
-        } elseif ($input instanceof self) {
-            $this->array = $input->array;
-        } else {
-            $this->array = [$input];
-        }
-    }
-
-    public function equals(array|self $other): bool
+    public static function get(array &$array, string $key, mixed $default = null): mixed
     {
-        $other = is_array($other) ? $other : $other->array;
-
-        return $this->array === $other;
-    }
-
-    /** @param Closure(mixed $value, mixed $key): bool $filter */
-    public function first(?Closure $filter = null): mixed
-    {
-        if ($filter === null) {
-            return $this->array[array_key_first($this->array)];
-        }
-
-        foreach ($this as $key => $value) {
-            if ($filter($value, $key)) {
-                return $value;
-            }
-        }
-
-        return null;
-    }
-
-    /** @param Closure(mixed $value, mixed $key): bool $filter */
-    public function last(?Closure $filter = null): mixed
-    {
-        if ($filter === null) {
-            return $this->array[array_key_last($this->array)];
-        }
-
-        foreach ($this->reverse() as $key => $value) {
-            if ($filter($value, $key)) {
-                return $value;
-            }
-        }
-
-        return null;
-    }
-
-    /** @param mixed $value The popped value will be stored in this variable */
-    public function pop(mixed &$value): self
-    {
-        $value = $this->last();
-
-        return new self(array_slice($this->array, 0, -1));
-    }
-
-    /** @param mixed $value The unshifted value will be stored in this variable */
-    public function unshift(mixed &$value): self
-    {
-        $value = $this->first();
-
-        return new self(array_slice($this->array, 1));
-    }
-
-    public function reverse(): self
-    {
-        return new self(array_reverse($this->array));
-    }
-
-    public function isEmpty(): bool
-    {
-        return empty($this->array);
-    }
-
-    public function isNotEmpty(): bool
-    {
-        return ! $this->isEmpty();
-    }
-
-    public function implode(string $glue): string
-    {
-        return implode($glue, $this->array);
-    }
-
-    public function values(): self
-    {
-        return new self(array_values($this->array));
-    }
-
-    /** @param Closure(mixed $value, mixed $key): bool $filter */
-    public function filter(Closure $filter): self
-    {
-        $array = [];
-
-        foreach ($this as $key => $value) {
-            if ($filter($value, $key)) {
-                $array[$key] = $value;
-            }
-        }
-
-        return new self($array);
-    }
-
-    /** @param Closure(mixed $value, mixed $key): void $each */
-    public function each(Closure $each): self
-    {
-        foreach ($this as $key => $value) {
-            $each($value, $key);
-        }
-
-        return $this;
-    }
-
-    /** @param Closure(mixed $value, mixed $key): mixed $map */
-    public function map(Closure $map): self
-    {
-        $array = [];
-
-        foreach ($this->array as $key => $value) {
-            $array[$key] = $map($value, $key);
-        }
-
-        return new self($array);
-    }
-
-    /** @param Closure(mixed $value, mixed $key): Generator $map */
-    public function mapWithKeys(Closure $map): self
-    {
-        $array = [];
-
-        foreach ($this->array as $key => $value) {
-            $generator = $map($value, $key);
-
-            if (! $generator instanceof Generator) {
-                throw new InvalidMapWithKeysUsage();
-            }
-
-            $array[$generator->key()] = $generator->current();
-        }
-
-        return new self($array);
-    }
-
-    /** @return mixed|ArrayHelper */
-    public function get(string $key, mixed $default = null): mixed
-    {
-        $value = $this->array;
-
         $keys = explode('.', $key);
 
         foreach ($keys as $key) {
-            if (! isset($value[$key])) {
+            if (! isset($array[$key])) {
                 return $default;
             }
 
-            $value = $value[$key];
+            $array = $array[$key];
         }
 
-        if (is_array($value)) {
-            return new self($value);
-        }
-
-        return $value;
+        return $array;
     }
 
-    public function has(string $key): bool
+    public static function has(array $array, string $key): bool
     {
-        $array = $this->array;
-
         $keys = explode('.', $key);
 
         foreach ($keys as $key) {
@@ -207,17 +36,8 @@ final class ArrayHelper implements Iterator, ArrayAccess, Serializable, Countabl
         return true;
     }
 
-    public function contains(mixed $search): bool
+    public static function set(array &$array, string $key, mixed $value): array
     {
-        return $this->first(fn ($value) => $value === $search) !== null;
-    }
-
-    public function set(string $key, mixed $value): self
-    {
-        $array = $this->array;
-
-        $current = &$array;
-
         $keys = explode('.', $key);
 
         foreach ($keys as $i => $key) {
@@ -233,60 +53,61 @@ final class ArrayHelper implements Iterator, ArrayAccess, Serializable, Countabl
             unset($keys[$i]);
 
             // If we know this key is not an array, make it one.
-            if (! isset($current[$key]) || ! is_array($current[$key])) {
-                $current[$key] = [];
+            if (! isset($array[$key]) || ! is_array($array[$key])) {
+                $array[$key] = [];
             }
 
             // Set the context to this key.
-            $current = &$current[$key];
+            $array = &$array[$key];
         }
 
         // Pull the first key out of the array
         // and use it to set the value.
-        $current[array_shift($keys)] = $value;
+        $array[array_shift($keys)] = $value;
 
-        return new self($array);
-    }
-
-    public function unwrap(): self
-    {
-        $unwrapValue = function (string|int $key, mixed $value) {
-            if (is_int($key)) {
-                return [$key => $value];
-            }
-
-            $keys = explode('.', $key);
-
-            for ($i = array_key_last($keys); $i >= 0; $i--) {
-                $currentKey = $keys[$i];
-
-                $value = [$currentKey => $value];
-            }
-
-            return $value;
-        };
-
-        $array = [];
-
-        foreach ($this->array as $key => $value) {
-            $array = array_merge_recursive($array, $unwrapValue($key, $value));
-        }
-
-        return new self($array);
-    }
-
-    public function toArray(): array
-    {
-        return $this->array;
+        return $array;
     }
 
     /**
      * @template T
-     * @param class-string<T> $to
-     * @return self<T>
+     * @param T[]|T $value
+     *
+     * @return array<T>
      */
-    public function mapTo(string $to): self
+    public static function wrap(mixed $value): array
     {
-        return new self(map($this->array)->collection()->to($to));
+        if (is_array($value)) {
+            return $value;
+        }
+
+        return [$value];
+    }
+
+    public function unwrap(array $array): array
+    {
+        $result = [];
+
+        foreach ($array as $key => $value) {
+            $result = array_merge_recursive($result, $this->toArray($key, $value));
+        }
+
+        return $result;
+    }
+
+    public function toArray(string|int $key, mixed $value): array
+    {
+        if (is_int($key)) {
+            return [$key => $value];
+        }
+
+        $keys = explode('.', $key);
+
+        for ($i = array_key_last($keys); $i >= 0; $i--) {
+            $currentKey = $keys[$i];
+
+            $value = [$currentKey => $value];
+        }
+
+        return $value;
     }
 }
--- src/PathHelper.php
+++ src/PathHelper.php
@@ -6,31 +6,14 @@ namespace Tempest\Support;
 
 final readonly class PathHelper
 {
-    public static function make(string ...$paths): string
+    public static function make(string ...$parts): string
     {
-        // Split paths items on forward and backward slashes
-        $parts = array_reduce($paths, fn (array $carry, string $part) => [...$carry, ...explode('/', $part)], []);
-        $parts = array_reduce($parts, fn (array $carry, string $part) => [...$carry, ...explode('\\', $part)], []);
+        $path = implode('/', $parts);
 
-        // Trim forward and backward slashes
-        $parts = array_map(fn (string $part) => trim($part, '/\\'), $parts);
-        $parts = array_filter($parts);
-
-        // Glue parts together
-        $path = implode(DIRECTORY_SEPARATOR, $parts);
-
-        // Add DIRECTORY_SEPARATOR if first entry starts with forward- or backward slash
-        $firstEntry = $paths[0];
-        if (str_starts_with($firstEntry, '/') || str_starts_with($firstEntry, '\\')) {
-            $path = DIRECTORY_SEPARATOR . $path;
-        }
-
-        // Add DIRECTORY_SEPARATOR if last entry ends with forward- or backward slash
-        $lastEntry = $paths[count($paths) - 1];
-        if ((count($paths) > 1 || strlen($lastEntry) > 1) && (str_ends_with($lastEntry, '/') || str_ends_with($lastEntry, '\\'))) {
-            $path .= DIRECTORY_SEPARATOR;
-        }
-
-        return $path;
+        return str_replace(
+            ['//', '\\'],
+            DIRECTORY_SEPARATOR,
+            $path,
+        );
     }
 }
