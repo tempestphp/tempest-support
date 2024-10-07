e2718ed38a39fec8b569890e4ae7a8a833729df7
aidan-casey <aidan@caseyhouse.net>
2024-10-07 07:42:41 +0000

    feat: add named hasOne relation (#549)

--- src/LanguageHelper.php
+++ src/LanguageHelper.php
@@ -4,19 +4,33 @@ declare(strict_types=1);
 
 namespace Tempest\Support;
 
+use Countable;
+use function Tempest\get;
+use Tempest\Support\Pluralizer\Pluralizer;
+
 final class LanguageHelper
 {
     /**
      * @param string[] $parts
      */
-    public static function join(array $parts): string
+    public static function join(array|ArrayHelper $parts): string
     {
-        $last = array_pop($parts);
+        $parts = arr($parts)->pop($last);
 
-        if ($parts) {
-            return implode(', ', $parts) . ' ' . 'and' . ' ' . $last;
+        if ($parts->isNotEmpty()) {
+            return $parts->implode(', ') . ' ' . 'and' . ' ' . $last;
         }
 
         return $last;
+    }
+
+    public static function pluralize(string $value, int|array|Countable $count = 2): string
+    {
+        return get(Pluralizer::class)->pluralize($value, $count);
+    }
+
+    public static function singularize(string $value): string
+    {
+        return get(Pluralizer::class)->singularize($value);
     }
 }
